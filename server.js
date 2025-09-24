ee857f5adef225b4bfb608b416338bb4d4059d2f
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-24 08:41:32 +0545

    set up socket.io server

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 import express from 'express'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 const app = express();
 
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
-console.log(__dirname);
+app.use(express.static(path.join(__dirname, "public")));
+console.log(path.join(__dirname, "public"));
 
 app.get('/', (req, res) => {
-    res.send("This is the index");
+    res.render("index");
 });
 
+io.on("connection", (socket) => {
+    console.log("User Connected", socket.id);
+})
+
 const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 })
