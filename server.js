9d0d146b04d56595d4f27c915531f1da9b115d18
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-09-24 07:15:41 +0545

    setup EJS view engine and serve static files from public directory

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 import express from 'express'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+console.log(__dirname);
 
 app.get('/', (req, res) => {
     res.send("This is the index");
-})
+});
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
